5905ab8c5ce04255660e98f4a2ac7072b4622019
HF17 <nasytw@gmail.com>
2025-07-15 11:43:38 +0200

    feat: can pick absolute or relative

--- diffuse_boost/output/push_simulation_PESC/helper.py
+++ diffuse_boost/output/push_simulation_PESC/helper.py
@@ -17,11 +17,13 @@ def concatenate(files, output_filename):
     torch.save(dataset_combined, output_filename)
     print(f"Saved combined dataset with {dataset_combined.shape[0]} samples to {output_filename}") 
 
-def pick_best(file, output_filename, top):
+def pick_best(file, output_filename, top, percentage=True):
     # top € [0,1]
     dataset = torch.load(file)
     n = dataset.shape[0]
-    m = int(n*top)
+    m = top
+    if percentage:
+        m = int(n*top)
     metrics = compute_metrics(dataset)
     metrics_sorted = sorted([(metrics["min_dists"][index], index) for index in range(n)], key=lambda x: -x[0])
     indices = torch.tensor([pair[1] for pair in metrics_sorted[:m]])
@@ -34,9 +36,9 @@ if __name__ == "__main__":
     mode = "pick"
     if mode == "pick":
         #pick_best("output/push_simulation_PESC/2025-07-07/dataset_combined_96k.pt", "output/generated_sets/96k_best.pt", 0.2)
-        pick_best("output/generated_sets/generated_20250708_143518.pt", "output/generated_sets/transformer_300_best.pt", 0.03)
+        pick_best("diffuse_boost/output/push_simulation_PESC/combined/dataset_combined_182k.pt", "diffuse_boost/output/push_simulation_PESC/combined/dataset_pick_182k_best_25k.pt", 25000, False)
     else:
         concatenate(files = [
-            "output/push_simulation_PESC/2025-07-03/dataset_combined_sym.pt",
-            "output/push_simulation_PESC/2025-07-04/dataset_sym.pt"],
-            output_filename="output/push_simulation_PESC/2025-07-07/dataset_combined_96k.pt")
+            "diffuse_boost/output/push_simulation_PESC/combined/dataset_combined_96k.pt",
+            "diffuse_boost/output/push_simulation_PESC/2025-07-14/dataset_sym.pt"],
+            output_filename="diffuse_boost/output/push_simulation_PESC/combined/dataset_combined_182k.pt")
